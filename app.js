bbccfa02f412014c48f541897bfe41b61fab261b
Abdul Mannan <>
2024-08-23 06:15:44 +0500

    JS

---
+++ app.js
@@ -0,0 +1,57 @@
+// Q)1
+alert ("Welcome to Our Website");
+
+// Q)2
+alert ("Error! Please Enter a valid password") ;
+
+// Q)3 
+alert ("This is the assignment \n of Javascript");
+
+// Q)4
+alert ("This is the assignment");
+alert ("Last Date of submission is 23 Aug");
+
+// Q)7
+var username;
+
+// Q)8
+var myName = "Abdul Mannan";
+
+// Q)9
+   // a)
+var message;
+
+   // b)
+var message ="Hello World" ;
+
+   // c)
+alert (message);
+
+// Q10)
+var namee = "Abdul Mannan"
+var course = "Web-Development 1"
+var age = "17 years"
+var personal_info = "namee + course + age"
+alert ("My name is"+ namee)
+alert ("I am doing course of" + course )
+alert ("My age is" + age)
+
+// Q11)
+var email = "abdulmannan@gmail.com"
+alert ("email address is" + email);
+
+// Q12)
+console.log (email);
+
+// Q13)
+var text = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"; 
+alert (text);
+console.log (text);
+
+// Q14)
+var agee = "17 years"
+alert (agee);
+
+// Q16)
+var year = "2006"
+console.log (year);
