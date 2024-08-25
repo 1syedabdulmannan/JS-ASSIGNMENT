d0992c3f68ecd7d5a73d95096de8ba804b49df11
Abdul Mannan <>
2024-08-25 17:07:30 +0500

    JS

--- app.js
+++ app.js
@@ -54,5 +54,133 @@ var agee = "17 years"
 alert (agee);
 
 // Q16)
-var year = "2006"
-console.log (year);
+var birthyear = "2006"
+console.log (birthyear);
+
+// Q18)
+ var myname = "Abdul Mannan", Age = "17", Coursee = "Web-Development"
+ alert(myname,Age,Coursee)
+ console.log(myname,Age,Coursee)
+
+ // Q19)
+// Legal Variables
+// var man;
+// var $doll;
+// var p__an;
+// var omg;
+
+// // ILLEGAL Variables
+// var 'abc';
+// var 0123xyz;
+// var =0js;
+// var name;
+// var @mb c;
+
+// Q20)
+
+  // a)
+ document.write("<h1>Rules for naming JS variables</h1>" );
+  
+  //b)
+ document.write("Variable names can only contain underscore , Number , Dollar sign and Letters. For example $my_1stVariable")
+ 
+  //c)
+ document.write("Variables must begin with a Letter , Dollar sign or underscore . For example $name, _name or name")
+
+  //d)
+ document.write("Variable names are case Sensitive")
+
+  //e)
+ 
+  document.write("Variable names should not be JS Reserved Words Examples of such keywords include if, else, for, while, function and var.")
+
+  //Q21
+ var num1 = +prompt ("Enter a First Number for add");
+ var num2 = +prompt ("Enter a Second Number for add");
+ var result = console.log(num1 + num2);
+ 
+  // Q22
+//Subtract
+ var num1 = +prompt("Enter a First Number for subtraction");
+ var num2 = +prompt("Enter a Second Number for subtraction");
+ var result1 = console.log(num1 - num2);
+
+ //Multiply
+ var num1 = +prompt("Enter a First Number for Multiply");
+ var num2 = +prompt("Enter a Second Number for Multiply");
+ var result2 = console.log(num1 * num2);
+
+ //Divide
+var num1 = +prompt("Enter a First Number for Divide");
+var num2 = +prompt("Enter a Second Number for Divide");
+var resul3 = console.log(num1 / num2);
+
+ //Modulus
+var num1 = +prompt("Enter a First Number for Modulus");
+var num2 = +prompt("Enter a Second Number for Modulus");
+var resul3 = console.log(num1 % num2);
+
+  //Q23
+
+ //a 
+var a
+
+ //b
+var b = 3 
+document.write("Value after variable declaration is: " + b + "<br>");
+
+ //c
+var c = 5;
+
+// //d
+ document.write("Initial value: " + c + "<br>");
+
+//e
+a++;
+
+//f
+console.log("Value after increment is:",a )
+
+//g
+var g =+ 8;
+
+//h
+ document.write("Value after addition is: " + g + "<br>");
+
+//i
+a--
+
+//j
+console.log("Value after decrement is:",a )
+
+ //k
+ var c = b % 3;
+ var remainder = c % 3;
+
+ //l
+ console.log("The remainder is: ",remainder)
+
+ // Q24
+var ticket = 600;
+var cost_of_five_tickets = 600 * 5;
+console.log(cost_of_five_tickets);
+
+ //Q25
+
+alert("Table of 5")
+
+document.write("<b>Table of 5</b><br/>")
+
+var a = 5
+
+document.write("<br/>5 x 1 =" + a*1)
+document.write("<br/>5 x 2 =" + a*2)
+document.write("<br/>5 x 3 =" + a*3)
+document.write("<br/>5 x 4 =" + a*4)
+document.write("<br/>5 x 5 =" + a*5)
+document.write("<br/>5 x 6 =" + a*6)
+document.write("<br/>5 x 7 =" + a*7)
+document.write("<br/>5 x 8 =" + a*8)
+document.write("<br/>5 x 9 =" + a*9)
+document.write("<br/>5 x 10 =" + a*10)
+
